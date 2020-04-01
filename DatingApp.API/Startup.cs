53518a51065406a8974ffdc23aa277d74a88b7f9
hashbond <tharinduhash5@gmail.com>
2020-04-01 23:59:15 +0530

    01-04-2020 Code Commit
    01-04-2020 Code Commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -34,6 +34,8 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite
             (Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            //this is we using to register our repo because create one instance inside scope
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
