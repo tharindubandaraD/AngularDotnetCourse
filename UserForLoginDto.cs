5122048680084b186e76a34a34f657622d741403
hashbond <tharinduhash5@gmail.com>
2020-04-01 23:53:55 +0530

    Delete UserForLoginDto.cs

--- UserForLoginDto.cs
+++
@@ -1,9 +0,0 @@
-namespace DatingApp.API.Dtos
-{
-    //created Dto to login
-    public class UserForLoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
