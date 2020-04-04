02c27124f5100dee08bd629f75e0cdd55d241357
Tharindu Hasintha Bandara <40268850+tharinduHasinthaBandaraDissanayaka@users.noreply.github.com>
2020-04-04 18:22:51 +0530

    06-04-2020 Parent to Child Communication Angular

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,17 +4,21 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { from } from 'rxjs';
 import { NavComponent } from './nav/nav.component';
 import { AuthService} from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 // it should have decarate with this
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-// use dependancy injection to use http client
-export class ValueComponent implements OnInit {
-  // this provides values what we getting from api - to use need to move to html page
-  values: any;
+export class HomeComponent implements OnInit {
+
+  registerMode = false;
+  values: any = {};
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getValues();
   }
-
+  registerToggle() {
+      this.registerMode = !this.registerMode;
+  }
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(respose => {
       this.values = respose;
