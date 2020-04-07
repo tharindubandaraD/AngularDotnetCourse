3c75763e9761224369d62b1a28dac47f972f4681
Tharindu Hasintha Bandara <40268850+tharinduHasinthaBandaraDissanayaka@users.noreply.github.com>
2020-04-07 20:30:23 +0530

    07-04-2020 Single Guard Angular

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -5,11 +5,19 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
+// add gaurd for portected multiple routs
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+                    { path: 'members', component: MemberListComponent},
+                    { path: 'messages', component: MessagesComponent},
+                    { path: 'lists', component: ListsComponent}
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
