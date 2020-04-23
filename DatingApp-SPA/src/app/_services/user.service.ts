091898654ffaed9b5ec232a719b6ddd01bb42d71
Tharindu Hasintha Bandara <40268850+tharinduHasinthaBandaraDissanayaka@users.noreply.github.com>
2020-04-24 00:32:10 +0530

    24-04-2020 Like Function in SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -68,4 +68,9 @@ export class UserService {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id );
   }
 
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
+
+
 }
